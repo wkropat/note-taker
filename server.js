8fd5ba779d6227d71f752b5ea426be564ba0505b
wkropat <wkropat@gmail.com>
2021-10-21 17:41:02 -0700

    updating for heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-
 // First import everything needed
 const express = require("express");
 const app = express();
 const path = require("path");
 const db = require("./db/db.json");
 const fs = require("fs");
-const PORT = 3000;
+const { randomUUID } = require("crypto");
+const { text } = require("express");
+
+// Heroku-friendly PORT
+const PORT = process.env.PORT || 3000;
 
 // Add in the middleware
 
@@ -15,20 +18,20 @@ app.use(express.json());
 
 // Create HTML routes
 
-app.get("/", (req,res)=> {
-    // GET / should return index.html file
-    // Question for the grader:
-    // Do I need this if I'm doing the same thing below with the app.get /* ?
-    res.sendFile(path.join(__dirname,`./public/index.html`));
-});
+// app.get("/", (req,res)=> {
+//     // GET / should return index.html file.
+//     // Question for the grader:
+//     // Do I need this if I'm doing the same thing below with the app.get /* ?
+//     res.sendFile(path.join(__dirname,`./public/index.html`));
+// });
 
 app.get("/notes",  (req,res)=>{
     // GET /notes should return the notes.html file.
     res.sendFile(path.join(__dirname,`./public/notes.html`));
 });
 
-// GET * should return the index.html file. Catchall function to return to index.html. Put last??
 app.get('/*', (req,res) => {
+    // Catchall function to return to index.html. Put last.
     res.sendFile(path.join(__dirname,`./public/index.html`));
 });
 
@@ -43,15 +46,25 @@ app.get("/api/notes",(req,res)=>{
 
 app.post("/api/notes", (req,res)=>{
     // Get UUID
-
+    uuid = randomUUID();
+    console.log(uuid);
     // POST /api/notes should receive a new note to save on the request body, 
-
+    // Make a new note using request
+    const note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid,
+    }
     // add it to the db.json file,
-
+    db.push(note);
+    fs.writeFilesSync("./db/db.json", JSON.strinigfy(db,null,4));
     // and then return the new note to the client
+
+    // Answer your questions.
+    res.send("Note taken.");
 });
 
 // Have to listen in to the port
 app.listen(PORT, () => {
-    console.log(`Smooth vibes coming through ${PORT}`);
+    console.log(`Smooth vibes coming through http://localhost:${PORT}`);
 });
