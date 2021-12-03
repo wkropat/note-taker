0e3c60edec966358ccd1addded0ef83156e497d6
wkropat <wkropat@gmail.com>
2021-12-02 17:48:36 -0800

    the fatal error was with the json file...

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 
-// My Routes 
+// I should use there routes instead:
 
 // const notes = require('./routes/noteRoutes');
 // app.use('/notes',notes);
@@ -20,26 +20,19 @@ app.use(express.json());
 // const api = require('./routes/apiRoutes');
 // app.use('/api/notes',api);
 
-// Heroku-friendly PORT process.env.PORT || 
-const PORT = process.env.PORT || 3011;
+// Heroku-friendly PORT 
+const PORT = process.env.PORT || 3001;
 
 // Create HTML routes
 
 app.get("/", (req,res)=> {
     // GET / should return index.html file.
-    // Question for the grader:
-    // Do I need this if I'm doing the same thing below with the app.get /* ?
     res.sendFile(path.join(__dirname,`./public/index.html`));
 });
 
 app.get("/notes",  (req,res)=>{
     // GET /notes should return the notes.html file.
     res.sendFile(path.join(__dirname,`./public/notes.html`));
-});
-
-app.get('/*', (req,res) => {
-    // Catchall function to return to index.html. Put last.
-    res.sendFile(path.join(__dirname,`./public/index.html`));
 });
 
 // Create API routes
@@ -54,19 +47,16 @@ app.get("/api/notes",(req,res)=>{
 
 // API Post route 
 
-app.post("/api/notes", (req,res)=>{
-    // Get UUID
-    
+app.post("/api/notes", (req,res)=>{ 
     // POST /api/notes should receive a new note to save on the request body, 
     // Make a new note using request
     const { title, text } = req.body;
     const newNote = { title, text,  id: uuidv4()};
-
-    // notesDb.push(req.body);
-    const noteString = JSON.stringify(notesDb);
-    fs.writeFile(`./db/db.json`, noteString, (err) =>
-      err ? console.error(err) : console.log("success")
-    );
+    const noteString = JSON.stringify(newNote);
+    readAndAppend(newNote, './db/db.json')
+    // fs.writeFile(`./db/db.json`, notesDb, (err) =>
+    //   err ? console.error(err) : console.log("success")
+    // );
     res.status(201).json(newNote);
     
 });
